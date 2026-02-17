18a4a90d9e5efd87448a12258b21cad917442d53
Ruthe46 <ruthinlosh.web.solwyz@gmail.com>
2026-02-18 00:47:26 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-[20px]'>Testing app</div>
+    </>
+  )
+}
+
+export default App
